e9d9cdfe85be2297bd634f22f00f7221106643c7
Gaiden-Spence <gaiden.anime@protonmail.com>
2025-07-12 21:28:49 -0400

    added dimensions of matrix as parameter

--- src/main.zig
+++ src/main.zig
@@ -9,13 +9,13 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     print("\n=== METHOD 5: Set rows with mixed types ===\n", .{});
-    const shape5 = [_]usize{ 3, 3 };
-    var matrix5 = try zn.Array(f64).init(allocator, &shape5);
+    var matrix5 = try zn.Array(f64).init(allocator, &[_]usize{ 3, 3 });
     defer matrix5.deinit();
 
     matrix5.set_row(0, .{ 1, 2.2, 4 }); // int + float
     matrix5.set_row(1, .{ 3.3, 4, 7 }); // float + int
     matrix5.set_row(2, .{ 5, 6.6, 9 }); // int + float
 
+    matrix5.scalar(2.5);
     matrix5.print_matrix();
 }
